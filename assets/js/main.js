ea6cb1d90f60ff95469e10a0704eaf68f1e28ce4
WNB-WNB <wnb_915@163.com>
2025-10-23 17:34:13 +0800

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,4 @@ const topBtn = document.getElementById('scroll-top');
 window.addEventListener('scroll', () => {
   topBtn.classList.toggle('show', window.scrollY > 300);
 });
-topBtn.addEventListener('click', () => window.scrollTo({ top: 0, behavior: 'smooth' }));
+topBtn.addEventListener('click', () => window.scrollTo({ top: 1000, behavior: 'smooth' }));
